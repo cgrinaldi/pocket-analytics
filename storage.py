4fdceaca19ee85eb95908e75dd4590d0c5391019
Chris Rinaldi <cgrinaldi@gmail.com>
2018-03-26 01:45:32 +0000

    Run load.py via cron job
    To get this to work, I had to use absolute paths and add the path
    to the Python interpreter.

--- storage.py
+++ storage.py
@@ -1,17 +1,23 @@
+import os
 import pandas as pd
 
 
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+SINCE_PATH = os.path.join(DIR_PATH, 'data/since.txt')
+ARTICLES_PATH = os.path.join(DIR_PATH, 'data/articles.csv')
+
+
 def record_data_pull_time(timestamp):
-    with open('data/since.txt', 'a+') as f:
+    with open(SINCE_PATH, 'a+') as f:
         f.write('{}\n'.format(timestamp))
 
 
 def read_times_api_queried():
     try:
-        with open('data/since.txt', 'r+') as f:
+        with open(SINCE_PATH, 'r+') as f:
             sinces = f.readlines()
             return [s.split('\n')[0] for s in sinces]
-    except FileNotFoundError:
+    except:
         return []
 
 
@@ -24,9 +30,9 @@ def get_most_recent_since():
 
 def save_articles(articles_new):
     try:
-        articles_prev = pd.read_csv('data/articles.csv')
+        articles_prev = pd.read_csv(ARTICLES_PATH)
         articles = pd.concat([articles_prev, articles_new])
         articles_deduped = articles.drop_duplicates(subset=['resolved_id'])
-        articles_deduped.to_csv('data/articles.csv', index=False)
-    except FileNotFoundError:
-        articles_new.to_csv('data/articles.csv', index=False)
+        articles_deduped.to_csv(ARTICLES_PATH, index=False, encoding='utf8')
+    except:
+        articles_new.to_csv(ARTICLES_PATH, index=False, encoding='utf8')
